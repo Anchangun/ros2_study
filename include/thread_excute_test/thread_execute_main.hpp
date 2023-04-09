cac1f71643c43d444ed249068bb84e63f2a5caac
Anchangun <changun516@gmail.com>
2023-04-09 22:59:24 +0900

    msg_callback_topic revise

--- include/thread_excute_test/thread_execute_main.hpp
+++ include/thread_excute_test/thread_execute_main.hpp
@@ -4,16 +4,21 @@
 #include <memory>
 #include <iostream>
 #include <unistd.h>
+#include <signal.h>
 // 
 #include "std_msgs/msg/string.hpp"
 #include "rclcpp/rclcpp.hpp"
+using std::placeholders::_1;
 
 class thread_execute_main : public rclcpp::Node{
     public : 
         thread_execute_main();
     private :
+        rclcpp::Subscription<std_msgs::msg::String>::SharedPtr sub_msg_one;
+        rclcpp::Subscription<std_msgs::msg::String>::SharedPtr sub_msg_two;
         void msg_one_callback(const std_msgs::msg::String::SharedPtr msg);
         void msg_two_callback(const std_msgs::msg::String::SharedPtr msg);
 };
+
 
 #endif
--- src/thread_execute_main.cpp
+++ src/thread_execute_main.cpp
@@ -1,20 +1,22 @@
 #include "thread_excute_test/thread_execute_main.hpp"
 thread_execute_main::thread_execute_main():Node("thread_execute_main"){
-
-
+    sub_msg_one = this->create_subscription<std_msgs::msg::String>("test_one",10,std::bind(&thread_execute_main::msg_one_callback,this,_1));
+    sub_msg_two = this->create_subscription<std_msgs::msg::String>("test_two",10,std::bind(&thread_execute_main::msg_two_callback,this,_1));
 }
 
 void thread_execute_main::msg_one_callback(const std_msgs::msg::String::SharedPtr msg){
     while(true){
         RCLCPP_INFO(this->get_logger(),"msg_one_callback : %s",msg->data.c_str());
-        sleep(100);
+        sleep(1);
+        signal(SIGINT,SIG_DFL);
     }
 }
 
 void thread_execute_main::msg_two_callback(const std_msgs::msg::String::SharedPtr msg){
     while(true){
         RCLCPP_INFO(this->get_logger(),"msg_two_callback : %s",msg->data.c_str());
-        sleep(100);
+        sleep(1);
+        signal(SIGINT,SIG_DFL);
     }
 }
 
